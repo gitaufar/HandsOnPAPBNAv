63a46c7b25c92037a6cd2d5a24bdfd037ab9ea28
gitpaja <ahmadafnani905@gmail.com>
2025-10-15 14:12:35 +0700

    Screen utama

--- app/src/main/java/com/example/papbnav/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/papbnav/ui/screens/DetailScreen.kt
@@ -1,2 +1,37 @@
 package com.example.papbnav.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun DetailScreen(navController: NavController, id: String?) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Detail Item", style =
+            MaterialTheme.typography.headlineSmall)
+        Spacer(Modifier.height(12.dp))
+        Text("ID diterima: $id", style =
+            MaterialTheme.typography.bodyLarge)
+        Spacer(Modifier.height(20.dp))
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Kembali")
+        }
+    }
+}
